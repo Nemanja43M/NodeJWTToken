f3311a2ac36878f497892bd6a74ce98be6fb9250
Nemanja43M <nemanjaamarkovic91@gmail.com>
2023-05-08 23:18:12 +0200

    Implementing authentication roles and permission to routes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const blogController = require("../controllers/blogController");
 const router = express.Router();
+const authController = require("./../controllers/authController");
 
 router.route("/blog-group").get(blogController.getBlogGroup);
 
 router
   .route("/")
-  .get(blogController.getAllBlogs)
-  .post(blogController.createBlog);
+  .get(
+    authController.protect,
+    authController.restrictTo("Admin", "Moderator", "Guest"),
+    blogController.getAllBlogs
+  )
+  .post(
+    authController.protect,
+    authController.restrictTo("Admin", "Moderator"),
+    blogController.createBlog
+  );
 
 router
   .route("/:id")
-  .get(blogController.getBlog)
-  .patch(blogController.updateBlog)
-  .delete(blogController.deleteBlog);
+  .get(
+    authController.protect,
+    authController.restrictTo("Admin", "Moderator", "Guest"),
+    blogController.getBlog
+  )
+  .patch(
+    authController.protect,
+    authController.restrictTo("Admin", "Moderator"),
+    blogController.updateBlog
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo("Admin", "Moderator"),
+    blogController.deleteBlog
+  );
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,13 +8,33 @@ router.post("/login", authController.login);
 
 router
   .route("/")
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
+  .get(
+    authController.protect,
+    authController.restrictTo("Admin"),
+    userController.getAllUsers
+  )
+  .post(
+    authController.protect,
+    authController.restrictTo("Admin"),
+    userController.createUser
+  );
 
 router
   .route("/:id")
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+  .get(
+    authController.protect,
+    authController.restrictTo("Admin"),
+    userController.getUser
+  )
+  .patch(
+    authController.protect,
+    authController.restrictTo("Admin"),
+    userController.updateUser
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo("Admin"),
+    userController.deleteUser
+  );
 
 module.exports = router;
